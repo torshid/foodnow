646cc948b8bddf0fbbf2352e89bed3b675b47f00
torshid <yaksoy@live.fr>
2016-12-10 17:57:43 +0300

    fix

--- entities/home.py
+++ entities/home.py
@@ -15,7 +15,7 @@ def reset():
         with connection.cursor() as cursor:
             try:
                 cursor.execute("""DROP TABLE IF EXISTS users""")
-                cursor.execute("""CREATE TABLE users (id SERIAL, mail VARCHAR UNIQUE, password VARCHAR)""")
+                cursor.execute("""CREATE TABLE users (id SERIAL, name VARCHAR, mail VARCHAR UNIQUE, password VARCHAR)""")
             except dbapi2.Error:
                 connection.rollback()
             else:
