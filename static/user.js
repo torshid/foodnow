7156d983a0b420860b570db81909d20080095eb1
smalaboy <smoctar12@gmail.com>
2016-12-10 17:42:54 +0200

    user page javascript file

--- static/user.js
+++ static/user.js
@@ -8,6 +8,5 @@ $(".btn-pref .btn").click(function () {
     // $(".tab").addClass("active"); // instead of this do the below
     $(this).removeClass("btn-default").addClass("btn-primary");
 
-
 });
 });
