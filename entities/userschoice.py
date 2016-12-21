b7fa487e0bd32e09cd9341f84501f6813e47b3f2
RealDavid <davidmusngzi@gmail.com>
2016-12-21 17:08:18 +0300

    Top foods by users discussion page

--- entities/userschoice.py
+++ entities/userschoice.py
@@ -7,25 +7,7 @@ from tables import users
 
 page = Blueprint(__name__)
 
-@page.route('/user/<int:user_id>')
+
+@page.route('/userschoice/')
 def main(user_id):
-    return render_template('user.html', user_id = user_id)
-
-def updateProfile(userId):
-    with db() as connection:
-        with connection.cursor() as cursor:
-            query = """UPDATE users
-                            SET"""
-            try:
-                cursor.execute(query);
-            except dbapi2.Error:
-                connection.rollback()
-            else:
-                connection.commit()
-    return
-
-def reset():
-    users.reset()
-    users.addUser('Yusuf Aksoy', 'yusuf@y.y', md5Password('12345'))  # id=1
-    users.addUser('Moctar Sawadogo', 'moctar@m.m', md5Password('12345'))  # id=2
-    return
+    return "HELLO AGAIN"
