cef42573db94e309f715defaa8f911d57b921265
torshid <yaksoy@live.fr>
2017-01-06 15:51:36 +0300

    elephant fix

--- server.py
+++ server.py
@@ -37,6 +37,16 @@ def error(e):
 def error(e):
     return render_template('errors/' + str(e.code) + '.html'), e.code
 
+def get_elephantsql_dsn(vcap_services):
+    """Returns the data source name for ElephantSQL."""
+    parsed = json.loads(vcap_services)
+    uri = parsed["elephantsql"][0]["credentials"]["uri"]
+    match = re.match('postgres://(.*?):(.*?)@(.*?)(:(\d+))?/(.*)', uri)
+    user, password, host, _, port, dbname = match.groups()
+    dsn = """user='{}' password='{}' host='{}' port={}
+             dbname='{}'""".format(user, password, host, port, dbname)
+    return dsn
+
 if __name__ == '__main__':
     app.secret_key = 'flask key'
     VCAP_APP_PORT = os.getenv('VCAP_APP_PORT')
@@ -52,13 +62,3 @@ if __name__ == '__main__':
         app.config['dsn'] = "user='" + dbuser + "' password='" + dbpass + "' host='localhost' port=5432 dbname='" + dbname + "'"
 
     app.run(host = '0.0.0.0', port = port, debug = debug)
-
-def get_elephantsql_dsn(vcap_services):
-    """Returns the data source name for ElephantSQL."""
-    parsed = json.loads(vcap_services)
-    uri = parsed["elephantsql"][0]["credentials"]["uri"]
-    match = re.match('postgres://(.*?):(.*?)@(.*?)(:(\d+))?/(.*)', uri)
-    user, password, host, _, port, dbname = match.groups()
-    dsn = """user='{}' password='{}' host='{}' port={}
-             dbname='{}'""".format(user, password, host, port, dbname)
-    return dsn
